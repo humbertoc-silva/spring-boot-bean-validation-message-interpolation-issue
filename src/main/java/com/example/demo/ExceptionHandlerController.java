2f7b59d5be01cb2fc3d9ce5ecb077ccae89ecee0
Humberto Corrêa da Silva <humbertoc_silva@hotmail.com>
2024-10-17 16:34:34 -0300

    Update example

--- src/main/java/com/example/demo/ExceptionHandlerController.java
+++ src/main/java/com/example/demo/ExceptionHandlerController.java
@@ -1,9 +1,34 @@
 package com.example.demo;
 
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.lang.Nullable;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 
 @RestControllerAdvice
 public class ExceptionHandlerController extends ResponseEntityExceptionHandler {
+
+    @Override
+    @Nullable
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(
+            MethodArgumentNotValidException ex, HttpHeaders headers, HttpStatusCode status, WebRequest request) {
+
+        if (ex.hasFieldErrors()) {
+            HashMap<String, String> errors= new HashMap<>();
+            ex.getFieldErrors().forEach(fieldError -> errors.put(fieldError.getCode(), fieldError.getDefaultMessage()));
+            ex.getBody().setProperty("errors", errors);
+        }
+
+        return handleExceptionInternal(ex, null, headers, status, request);
+    }
 
 }
